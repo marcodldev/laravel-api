621bc2e46982d9f99d0e0e9079f58da83b078c56
marcodldev <marcodelisi2@gmail.com>
2023-02-01 18:01:22 +0100

    + import vue

---
+++ resources/js/views/App.vue
@@ -0,0 +1,42 @@
+<template>
+	<div>
+
+    <WorkInProgress />
+
+    <div>
+        <ul>
+            <li v-for="elem in posts" :key="elem.id">{{ elem.title }}</li>
+        </ul>
+    </div>
+	</div>
+</template>
+
+<script>
+
+import WorkInProgress from "../components/WorkInProgress.vue"
+
+export default{
+	name: "App",
+	components: {
+
+        WorkInProgress
+	//inserire futuri componenti
+	},
+    data(){
+        return{
+            posts: []
+        }
+    },
+    mounted(){
+        this.getPosts();
+    },
+    methods : {
+        getPosts(){
+            axios.get('http://localhost:8000/api/posts')
+                .then((res) => {
+                    this.posts = res.data
+                })
+        }
+    }
+}
+</script>
